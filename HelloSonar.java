5e8e7f4d256c9306b2896a9756c304ba666e8c97
jdhason <jimsonmorais@gmail.com>
2017-03-29 17:01:19 +0530

    this is initial version of sonar test

--- HelloSonar.java
+++ HelloSonar.java
@@ -2,7 +2,9 @@ public class HelloSonar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java... Welcome to Sonar");
+		System.out.println("Hello Java0... Welcome to Sonar0");
+		//System.out.println("Hello Java1... Welcome to Sonar1");
+		//System.out.println("Hello Java2... Welcome to Sonar2");
 
 	}
 
