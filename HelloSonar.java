4b5a7644ace639b91ba2055bd7798cb9c8782edb
jdhason <jimsonmorais@gmail.com>
2017-04-04 10:52:14 +0530

    Update HelloSonar.java

--- HelloSonar.java
+++ HelloSonar.java
@@ -2,7 +2,8 @@ public class HelloSonar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java... Welcome to Sonar");
+		System.out.println("Hello Java1... Welcome to Sonar1");
+		System.out.println("Hello Java2... Welcome to Sonar2");
 
 	}
 
